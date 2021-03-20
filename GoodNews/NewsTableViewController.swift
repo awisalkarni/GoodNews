b539c1bb0692bca5e0f7276fd73c03debace1baf
awis alkarni <awis@awislabs.com>
2021-03-20 16:38:04 +0800

    initial commit with pod

---
+++ GoodNews/NewsTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTableViewController.swift
+//  GoodNews
+//
+//  Created by Awis Alkarni on 20/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
